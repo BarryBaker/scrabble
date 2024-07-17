ff2e79e42bd609d6fc9ed67f5dbe4cfbc040a14e
BarryBaker <megergo@yahoo.com>
2024-07-17 02:57:04 +0200

    oiio

--- src/App.vue
+++ src/App.vue
@@ -219,7 +219,7 @@
       // const wsUrl = `${protocol}://${host}`;
       // this.socket = new WebSocket(process.env.VUE_APP_BASE_URL);
       this.socket = new WebSocket(
-        "wss://a4aa-2001-1c00-2625-f700-19d-3ca1-cef8-488e.ngrok-free.app/"
+        "wss://a4aa-2001-1c00-2625-f700-19d-3ca1-cef8-488e.ngrok-free.app"
       );
 
       this.socket.onmessage = this.sockets.handleMessage.bind(this);
