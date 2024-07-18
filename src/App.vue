caeb654a10793d6e25bc46924e70824f6cd4f6ed
BarryBaker <megergo@yahoo.com>
2024-07-18 14:46:51 +0200

    pok

--- src/App.vue
+++ src/App.vue
@@ -218,9 +218,7 @@
       // const host = window.location.host;
       // const wsUrl = `${protocol}://${host}`;
       // this.socket = new WebSocket(process.env.VUE_APP_BASE_URL);
-      this.socket = new WebSocket(
-        "wss://https://716e-80-114-243-74.ngrok-free.app/"
-      );
+      this.socket = new WebSocket("wss://716e-80-114-243-74.ngrok-free.app/");
 
       this.socket.onmessage = this.sockets.handleMessage.bind(this);
       this.socket.onopen = () => {
